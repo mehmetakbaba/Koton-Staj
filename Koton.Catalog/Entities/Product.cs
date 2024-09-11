2dab8fe1f5f67dbc5c2ecebede19e5d8d50e4690
Mehmet <mehmetaakbaba@gmail.com>
2024-09-11 16:20:25 +0300

    proje create edildi

---
+++ Koton.Catalog/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Koton.Catalog.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; } = default!;
+        public string ProductName { get; set; } = default!;
+        public decimal ProductPrice { get; set; }
+        public decimal ProductQuantity { get; set; }
+    }
+}
